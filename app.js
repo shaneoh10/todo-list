f77c47e69ff78b079e39fdc15e1037b5e94f747c
Shane O'Hanlon <shaneaghada@gmail.com>
2021-11-12 11:00:42 +0000

    Set up ejs partials and about page

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { request } = require('express');
 
 const app = express();
 
 let newItems = [];
+let workItems = []
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
     let today = new Date();
@@ -18,14 +21,28 @@ app.get('/', function(req, res){
     }
     let date = today.toLocaleDateString('en-US', dateOptions);
 
-    res.render('list', {date: date, newItems: newItems});
+    res.render('list', {title: date, newItems: newItems});
 });
 
-app.post('/', function(req, res) {
+app.post('/', function(req, res){
     let item = req.body.newItem;
-    newItems.push(item);
-    res.redirect('/');
+
+    if(req.body.list === 'Work') {
+        workItems.push(item)
+        res.redirect('/work');
+    } else {
+        newItems.push(item);
+        res.redirect('/');
+    }
 })
+
+app.get('/work', function(req, res){
+    res.render('list', {title: 'Work List', newItems: workItems});
+});
+
+app.get('/about', function(req, res){
+    res.render('about');
+});
 
 app.listen(3000, function(){
     console.log('Server running on port 3000');
