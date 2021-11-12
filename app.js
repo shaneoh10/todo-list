194a935b5617938ab3fb942d4b8fb9469df86457
Shane O'Hanlon <shaneaghada@gmail.com>
2021-11-12 09:40:18 +0000

    Set up app.js to handle to do list

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let newItems = [];
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', function(req, res){
     let today = new Date();
-    const day = days[today.getDay()];
-    res.render('list', {day: day});
+    let dateOptions = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
+    let date = today.toLocaleDateString('en-US', dateOptions);
+
+    res.render('list', {date: date, newItems: newItems});
 });
+
+app.post('/', function(req, res) {
+    let item = req.body.newItem;
+    newItems.push(item);
+    res.redirect('/');
+})
 
 app.listen(3000, function(){
     console.log('Server running on port 3000');
